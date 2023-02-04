e57028e2eaebd72b2685d6fb18ecada4f0062587
muzkatnuzz <muzkatnuzz@outlook.com>
2023-02-04 20:49:06 +0100

    Replace recursive loop for waiting by waitFor

--- src/prometheus-exporter.ts
+++ src/prometheus-exporter.ts
@@ -5,6 +5,7 @@ import { modemFactory } from './modem/factory';
 import { discoverModemIp, ModemDiscovery } from './modem/discovery'
 import { Log } from './logger'
 import { getStatus } from './commands/status'
+import { resolve } from 'path';
 
 export class PrometheusExporter {
     protected readonly httpServer: Express.Application
@@ -18,7 +19,8 @@ export class PrometheusExporter {
         this.password = password
         this.scrapePort = 9705
         this.cookieJar = new CookieJar()
-        this.httpServer = this.initServer()
+        this.httpServer = Express()
+        this.initServer()
     }
 
     private async scrape() {
@@ -79,9 +81,7 @@ export class PrometheusExporter {
         scrapeSuccess.set('loginLogout', loginLogoutSuccess)
     }
 
-    private initServer():Express.Application  {
-        const metricServer =  Express() 
-
+    private initServer() {
         // init metric handling
         const collectDefaultMetrics = ({
             timeout: 10000,
@@ -115,33 +115,37 @@ export class PrometheusExporter {
             c.inc();
         }, 2000);
 
-        // recursive loop to not overlap scrapes
-        // src: https://developer.mozilla.org/en-US/docs/Web/API/setInterval
-        // TODO: review if resources are freed: 
-        // https://medium.com/@devinmpierce/recursive-settimeout-8eb953b02b98
-        var loop = (async() => {
-            // In an arrow function, the 'this' pointer is interpreted lexically,
-            // so it will refer to the object as desired.
-            await this.scrape()
-            setTimeout(()=>{
-                loop()
-            }, 10000) // 10 seconds between each scrape
-        });
-        loop();
-
         register.metrics().then(str => console.log(str));
 
         // magic to actually serve scraped values
-        metricServer.get('/metrics', (req, res) => {
+        this.httpServer.get('/metrics', (req, res) => {
             console.log('Scraped')
             res.send(register.metrics())
-          })
-          
-          metricServer.listen(this.scrapePort, () =>
-            console.log(`🚨 Prometheus listening on port ${this.scrapePort} /metrics`)
-          )
+        })
 
-        return metricServer
+        this.httpServer.listen(this.scrapePort, () =>
+            console.log(`🚨 Prometheus listening on port ${this.scrapePort} /metrics`)
+        )
+
+        // wait indefinitely
+        this.waitFor(false, 3000)
+    }
+
+    /**
+     * Sleep in intervalls until condition comes true
+     * @param condition condition to check between intervalls
+     * @param intervall intervall to sleep in between in ms
+     */
+    private async waitFor(condition: boolean, intervall: number): Promise<any> {
+        const poll = () => {
+            if (condition) {
+                resolve()
+            } else {
+                setTimeout(() => poll(), intervall)
+            }
+        };
+
+        return new Promise(poll)
     }
 
     /**
